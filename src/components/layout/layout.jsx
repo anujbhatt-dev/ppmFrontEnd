a7fb08a4de9f06ef21c10ca1cd1d106a558b6cf9
anuj bhatt <anujbhatt023@gmail.com>
2020-06-25 17:47:21 +0530

    added image to side drawer background

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -4,18 +4,16 @@ import SideDrawer from "../navigation/side-drawer/side-drawer";
 
 class Layout extends Component{
   state={
-    sideDrawer:"sideDrawer",
+    sideDrawer:["sideDrawer","close"],
     toolbar:["toolbar","closeToolbar"],
     toggle:true
   }
 
   sideDrawerToggleHandler=()=>{
-    console.log("in sideDrawerToggleHandler");
     const newToggle = !this.state.toggle
     this.setState({toggle:newToggle})
     const classes = ["sideDrawer",this.state.toggle?"open":"close"]
     const newClasses = classes.join(" ");
-    console.log(newClasses);
     this.setState({sideDrawer:newClasses})
     this.toolbarHandler();
   }
@@ -24,11 +22,9 @@ class Layout extends Component{
      const newToggle =this.state.toggle
      if(this.state.toggle){
        const newClasses = ["toolbar",newToggle?"openToolbar":"closeToolbar"]
-       console.log(newClasses);
        this.setState({toolbar:newClasses})
      }else{
        const newClasses = ["toolbar",newToggle?"openToolbar":"closeToolbar"]
-       console.log(newClasses);
        this.setState({toolbar:newClasses})
      }
    }
--- src/components/navigation/toolbar/nav-items/nav-item/nav-item.jsx
+++ src/components/navigation/toolbar/nav-items/nav-item/nav-item.jsx
@@ -1,4 +1,5 @@
- import React, {Component} from "react"
+ import React, {Component} from "react";
+ import {Link} from "react-router-dom";
 
 
  class NavItem extends Component{
@@ -7,8 +8,8 @@
 
      return (
       <div className="nav__items">
-         <div className="nav__items-item">signUp</div>
-         <div className="nav__items-item">logIn</div>
+         <Link to="/signup" className="nav__items-item">signUp</Link>
+         <Link to="/login" className="nav__items-item">logIn</Link>
       </div>
      )
    }
